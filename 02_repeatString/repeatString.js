832262eb67fd1133441a814bfa197b835193bd11
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-01 08:56:15 -0400

    Complete repeatString()

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    let rslt = '';
 
+    if (n<0) { rlst= 'ERROR'} else {
+    for (let i = 0; i<n; i++){
+        rslt+=str;
+        }
+    }
+return rslt;
 };
 
 // Do not edit below this line
