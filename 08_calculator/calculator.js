bdfaebec7c6f6bf494fcd5a7640d3bd68fba9a89
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-01 11:20:42 -0400

    Update functions and tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(x,p) {
+	return Math.pow(x,p);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0 || x ==1 ) return 1;
+  for (let i = x - 1; i >= 1; i--){
+    x *= i;
+  }
+  return x;
 };
 
 // Do not edit below this line
