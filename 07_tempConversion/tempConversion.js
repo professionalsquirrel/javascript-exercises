1288c4c6aafd13cad2169e348da454fa83bbb223
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-01 10:44:09 -0400

    Update function and tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  return (x - 32) * 5/9
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  return x * 9/5 + 32
 };
 
 // Do not edit below this line
