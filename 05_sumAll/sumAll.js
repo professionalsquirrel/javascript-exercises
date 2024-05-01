5a3062d945f3e7e6164911fa9f08c1dc7f2284fa
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-01 10:19:23 -0400

    Update function and tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(n,m) {
+    if (!Number.isInteger(n) || !Number.isInteger(m)) return "ERROR";
+    if (n<0 || m<0) return "ERROR";
+    if (n > m) [n, m] = [m, n];
+    let sum = 0;
+    for (let i=n; i<=m; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
