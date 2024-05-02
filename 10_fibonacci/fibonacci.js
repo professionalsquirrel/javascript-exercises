60159eaa4c820f4c3d4a6efb2c930db6059bb790
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-02 05:32:35 -0400

    Complete function and remove .skip from all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(rank) {
+    if (rank<0) return "OOPS";
+    //initialize an array with first two Fibonacci numbers
+    const f = [0,1];
+    //generate the rest of the Fibonacci sequence up to element 'rank'
+    for (let i = 2; i <= rank; i++){
+        f[i] = f[i-1] + f[i-2];
+    }
+    //return the number the user is asking for
+    return f[rank];
 };
 
 // Do not edit below this line
