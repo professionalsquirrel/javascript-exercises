85cc793ed3f817f3d5550f7741a2a44b267a5e95
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-02 05:00:21 -0400

    Complete function and remove .skip from all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let simplifiedString = str.trim().toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let reversedString = simplifiedString.split('').reverse().join('');
+    return simplifiedString === reversedString;
 };
 
 // Do not edit below this line
