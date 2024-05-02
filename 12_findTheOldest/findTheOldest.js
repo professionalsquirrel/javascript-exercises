d830e1d54a3256c65648308065ebe6c7c1466f58
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-02 09:31:50 -0400

    Modify function and remove .skip's from tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    //function to obtain person's age
+    function getAge (person){
+        //if the person has not died, this means we need to calculate their age using a JS method
+        if (!(person.yearOfDeath)){
+            currentYear = new Date().getFullYear();
+            return currentYear - person.yearOfBirth;
+        }
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+   return array.reduce((oldest, currentPerson) => { return (getAge(oldest) < getAge(currentPerson)) ? currentPerson : oldest;});
 };
 
 // Do not edit below this line
