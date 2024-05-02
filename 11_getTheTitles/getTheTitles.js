e16ff9ea1ff0b0c9801fd7fdfeb19394eaa2acfc
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-05-02 05:45:38 -0400

    Complete the function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(userArray) {
+    //map each element to just the title, into a new array
+    return userArray.map(element => element.title);
 };
 
 // Do not edit below this line
